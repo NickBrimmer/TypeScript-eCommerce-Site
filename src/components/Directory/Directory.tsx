828cf8fd877f84f72c005993fda9366671500cb1
Nicholas Brimmer <nicholasbrimmer@Nicholass-MacBook-Pro.local>
2020-10-13 17:37:01 -0700

    added images

--- src/components/Directory/Directory.tsx
+++ src/components/Directory/Directory.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { MenuItem } from '../MenuItem/MenuItem';
+import img1 from '../../assets/running.jpg'
+import img2 from '../../assets/basketball.png'
+import img3 from '../../assets/redSneaker.jpg'
+import img4 from '../../assets/trainers.jpg'
+import img5 from '../../assets/classics.jpg'
 
 import '../../styles/components/Directory.scss';
 
@@ -9,33 +14,33 @@ interface DirectoryProps {
 
 const sections = [
   {
-    title: 'hats',
-    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+    title: 'TRAINERS',
+    imageUrl: img4,
     id: 1,
     linkUrl: 'shop/hats'
   },
   {
-    title: 'jackets',
-    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+    title: 'CLASSICS',
+    imageUrl: img5,
     id: 2,
     linkUrl: 'shop/jackets'
   },
   {
-    title: 'sneakers',
-    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    title: 'SNEAKERS',
+    imageUrl: img3,
     id: 3,
     linkUrl: 'shop/sneakers'
   },
   {
-    title: 'womens',
-    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+    title: 'RUNNING',
+    imageUrl: img1,
     size: 'large',
     id: 4,
     linkUrl: 'shop/womens'
   },
   {
-    title: 'mens',
-    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    title: 'BASKETBALL',
+    imageUrl: img2,
     size: 'large',
     id: 5,
     linkUrl: 'shop/mens'
